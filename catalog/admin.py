52a78ebe43d426f6d79e5649085a0c6fbdb9c4a9
firestar555 <thomasyee40@gmail.com>
2020-04-22 16:19:11 -0600

    added a list filter to book instance admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -25,4 +25,5 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    # Used to filter which items are displayed
+    list_filter = ('status', 'due_back')
