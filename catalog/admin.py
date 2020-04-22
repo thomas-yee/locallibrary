4da36094f3e992409aab22d5dc1cb7b5336980ad
firestar555 <thomasyee40@gmail.com>
2020-04-22 15:55:37 -0600

    added a list view to author admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+
+# Define the admin class
+class AuthorAdmin (admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+# Register the admin classes for Book using the decorator
+# Does the same thing as admin.site.register()
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
