448bcfbed62d87552346b6d3fe2eec79c90bb62e
firestar555 <thomasyee40@gmail.com>
2020-04-22 14:04:32 -0600

    added bookinstance model

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+import uuid # Required for unique book instances
 
 # Create your models here.
 # Genre Model
@@ -39,3 +40,38 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    # ---- Fields ----
+    # UUIDField used to set id as primary key - Allocates a global unique value for each instance
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    # Key-pair values - Value is a display value the user can select
+    # Keys are the values that are actually saved if it selected
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On Loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length = 1,
+        choices = LOAN_STATUS,
+        blank = True,
+        default = 'm',
+        help_text = 'Book availability',
+    )
+
+    # Will be sorted alphabetically by due_back
+    class Meta:
+        ordering = ['due_back']
+
+    # ---- Methods ----
+    def __str__ (self):
+        """String for representing the Model Object."""
+        return f'{self.id} ({self.book.title})'
